69a2cdcd24c8b27a7fe6fcaa65656a21056f375a
Rhapsody Siu <rhapsody.siu@gmail.com>
2020-05-02 10:15:56 +0800

    solve conflicts in some UI components due to merging

--- Assets/Scripts/UI/SwitchSubActionBtn.cs
+++ Assets/Scripts/UI/SwitchSubActionBtn.cs
@@ -8,22 +8,46 @@ public class SwitchSubActionBtn : MonoBehaviour
     // Start is called before the first frame update
     private Button btn;
     private DebateModeController controller;
+    private DataController data;
     void Start()
     {
         controller = Object.FindObjectOfType<DebateModeController>();
-        btn = transform.GetComponent<Button>();
+        data = Object.FindObjectOfType<DataController>();
         if (btn == null)
         {
-            Debug.LogError("SwitchSubActoinBtn: cannot find button component");
+            Debug.LogError("SwitchSubActionBtn: cannot find button component");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    public void setText(string t)
+    {
+        Text txt = transform.Find("Text").GetComponent<Text>();
+        txt.text = t;
+    }
+
+
+    public void setFieldText(Argument argument, string option)
+    {
+        switch (option)
+        {
+            case "THESES":
+                if (argument.thesis != null) setText(argument.thesis.thesisName);
+                else setText(argument.argument.ArgumentName);
+                break;
+            case "TACTIC":
+                setText(argument.tactic.tacticName);
+                break;
+            case "FACT":
+                setText(argument.fact.factName);
+                break;
+        }
+    }
     public void SetRedirectTo(PreviewSubActionPanel.ToggleMenuOption option)
     {
         switch (option)
@@ -39,4 +63,5 @@ public class SwitchSubActionBtn : MonoBehaviour
                 return;
         }
     }
+
 }
